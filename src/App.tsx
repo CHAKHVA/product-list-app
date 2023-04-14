38cc34b0dad860bfe6a47402d8b3fe886664feee
Aleksandre Chakhvashvili <aleksandre.chakhvashvili24@gmail.com>
2023-04-14 21:54:33 +0400

    setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
+import ProductList from "./pages/ProductList/ProductList";
+import AddProduct from "./pages/AddProduct/AddProduct";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProductList />}/>
+          <Route path="/addproduct" element={<AddProduct />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
